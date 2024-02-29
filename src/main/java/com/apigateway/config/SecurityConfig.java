01108060cf9c314eec0c4cba61f3252c752e2308
dipesh kumar chaudhary <dipeshc045@gmail.com>
2024-02-29 19:01:23 +0545

    deflgd

---
+++ src/main/java/com/apigateway/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.apigateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
+
+                );
+        http.oauth2Client(withDefaults());
+
+        http.oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(withDefaults()));
+
+
+        return http.build();
+    }
+
+
+}
